d3d95ab564e42336f05d1cc50cfc83d6cf58fc23
TE\kseernani <kunal.seernani@technica-engineering.de>
2025-04-23 15:11:15 +0200

    commit yoyo

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -3,7 +3,7 @@ import db from "@repo/db/client";
 const app = express();
 
 app.use(express.json())
-
+///sgfrdegedrg
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
